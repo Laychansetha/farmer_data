645441652a8cbbd5e744386296e0f74a48d6094f
Chansetha <laychansetha@gmail.com>
2023-07-31 14:27:26 +0700

    Just add code

--- farmerdata.py
+++ farmerdata.py
@@ -1,4 +1,10 @@
 import streamlit as st
+from streamlit_extras.metric_cards import style_metric_cards
 
 st.title("Summary Farmer Data")
 
+col1, col2, col3 = st.columns(3)
+col1.metric(label="Gain", value=5000, delta=1000)
+col2.metric(label="Loss", value=5000, delta=-1000)
+col3.metric(label="No Change", value=5000, delta=0)
+style_metric_cards()
